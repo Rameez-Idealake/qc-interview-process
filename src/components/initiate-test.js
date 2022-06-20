689d469b1d3ac3abe02644215b2774f57dbfefa8
Rameez-Idealake <106642472+Rameez-Idealake@users.noreply.github.com>
2022-06-20 11:37:34 +0530

    Revert "changed initiate test"
    This reverts commit 5c4ae9f56ce2d719dfc0eb6d532f92fec431c3b4.

--- src/components/initiate-test.js
+++ src/components/initiate-test.js
@@ -3,7 +3,7 @@ const InitiateTest = () => {
     return (
         <div className='container'>
 
-            <h1>Initiate Test</h1>
+            <h1>Initiate Test text change</h1>
             
         </div>
 
